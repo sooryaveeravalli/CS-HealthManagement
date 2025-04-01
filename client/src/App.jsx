96dc9eb5260ddaff2c684cf2322f7cf9825afae2
Ujjwal Gupta <ujjwalgupta@Ujjwals-MacBook-Pro.local>
2025-03-31 22:58:49 -0400

    new files for hms

---
+++ client/src/App.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Home from "./Pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Appointment from "./Pages/Appointment";
+import QuickHelp from "./Pages/QuickHelp";
+import Login from "./Pages/login";
+import Register from "./Pages/Register";
+import RegisterDoctor from "./Pages/RegisterDoctor";
+import Loginadmin from "./Pages/loginadmin";
+import Logindoctor from "./Pages/logindoctor";
+import AdminHome from "./Pages/AdminHome";
+import MessagesDisp from "./Pages/MessagesDisp";
+import AddAdmin from "./Pages/AddAdmin";
+import AddDoctor from "./Pages/AddDoctor";
+import Doctors from "./Pages/Doctors";
+import { useContext } from "react";
+import { useEffect } from "react";
+import { Context } from "./main";
+import Test from "./Pages/test";
+import DoctorHome from "./Pages/DoctorHome";
+import PatientHome from "./Pages/PatientHome";
+
+
+import Testapp from "./test/testapp";
+
+const App = () => {
+  const { isAuthenticated, setIsAuthenticated, setUser } =
+    useContext(Context);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:8000/api/v1/users/patient/profile",
+          // "https://e-healthcare-management-system-2.onrender.com/users/patient/profile",
+          {
+            withCredentials: true,
+          }
+        );
+        setIsAuthenticated(true);
+        setUser(response.data.user);
+      } catch (error) {
+      }
+    };
+    fetchUser();
+  }, [isAuthenticated]);
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/appointment" element={<Appointment />} />
+          <Route path="/quick-help" element={<QuickHelp />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/logindoctor" element={<Logindoctor />} />
+          <Route path="/loginadmin" element={<Loginadmin />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/register-doctor" element={<RegisterDoctor />} />
+          <Route path="/admin" element={<AdminHome />} />
+          <Route path="/messages" element={<MessagesDisp />} />
+          <Route path="/doctor-addnew" element={<AddDoctor />} />
+          <Route path="/admin-addnew" element={<AddAdmin />} />
+          <Route path="/doctors" element={<Doctors />} />
+          <Route path="/test" element={<Test />} />
+          <Route path="/patient-home" element={<PatientHome />} />
+          <Route path="/doctor-home" element={<DoctorHome />} />
+          <Route path="/testapp" element={<Testapp/>} />
+        </Routes>
+        <ToastContainer position="top-center" />
+      </Router>
+    </>
+  );
+};
+
+export default App;
